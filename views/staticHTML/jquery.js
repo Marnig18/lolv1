04f2c4ccb5ad0e107f131f802b16c4e7bbb0fab1
Oscar Abella <oscarabella@Oscars-MacBook-Pro.local>
2017-07-12 15:55:24 -0400

    views

---
+++ views/staticHTML/jquery.js
@@ -0,0 +1,18 @@
+
+// $(document).ready(function(){  
+//     $('#myModal')
+// })
+// $(document).ready(function(){  
+//     $('#myModal').modal('show'); 
+// })
+
+$("#myModal").on("click", function(){
+	$("myModal").modal('show'); 
+})
+$("#navSignUp").on("click", function(){
+	$("navSignUp").modal('show'); 
+})
+
+$("#navSignUp").on("click", function(){
+	$("navSignUp").modal('show'); 
+})
